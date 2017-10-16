179d2a4c14725fdb32367a472cbe0bf83a0af9ab
Severin Wiggenhorn <severin.randall@gmail.com>
2017-10-16 12:18:31 -0700

    added create method to session controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -25,6 +25,24 @@ class SessionsController < ApplicationController
     redirect_to root_path
   end
 
+  def create
+    auth_hash = request.env['omniauth.auth']
+
+    if auth_hash['uid']
+      @user = User.find_by(uid: auth_hash[:uid], provider: 'github')
+      if @user.nil?
+        # User doesn't match anything in the DB
+        # Attempt to create a new user
+      else
+        flash[:success] = "Logged in successfully"
+        redirect_to root_path
+      end
+    else
+      flash[:error] = "Could not log in"
+      redirect_to root_path
+    end
+  end
+
   def logout
     session[:user_id] = nil
     flash[:status] = :success
